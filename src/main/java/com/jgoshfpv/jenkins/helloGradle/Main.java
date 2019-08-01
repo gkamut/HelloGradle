f8780c8e8c31c469c0c00acccf294e16b541cd1e
Grzegorz K <g.kamut@gmail.com>
2019-08-01 15:17:49 +0200

    Intentionaly made a bug.

--- src/main/java/com/jgoshfpv/jenkins/helloGradle/Main.java
+++ src/main/java/com/jgoshfpv/jenkins/helloGradle/Main.java
@@ -3,5 +3,6 @@ package com.jgoshfpv.jenkins.helloGradle;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Gradle");
+	System.out.println(14/0);
     }
 }

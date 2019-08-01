20b871b310853fb925fa4c98cf7beeb8a56fe488
Grzegorz K <g.kamut@gmail.com>
2019-08-01 15:39:29 +0200

    Fixed the code + added displaying current date time.

--- src/main/java/com/jgoshfpv/jenkins/helloGradle/Main.java
+++ src/main/java/com/jgoshfpv/jenkins/helloGradle/Main.java
@@ -1,8 +1,13 @@
 package com.jgoshfpv.jenkins.helloGradle;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Gradle");
-	System.out.println(14/0);
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+	LocalDateTime dateTime = LocalDateTime.now();
+	System.out.println(dateTime.format(formatter));
     }
 }
